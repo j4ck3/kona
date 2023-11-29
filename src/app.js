26451ac444ce7a7d3fe146df2f3e5f8f44ce6cae
j4ck3 <jacobhallgren@live.se>
2023-11-29 01:32:49 +0100

    matsmart

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const axios = require('axios');
 
-const { Client, IntentsBitField, EmbedBuilder, ActivityType } = require('discord.js')
+const { Client, IntentsBitField, EmbedBuilder, ActivityType, formatEmoji } = require('discord.js')
 
 
 const client = new Client({
@@ -18,10 +18,51 @@ client.on('ready', (c) => {
     console.log(`${c.user.username} is Online`)
 
     client.user.setActivity({
-        name: 'Q1 QMK ISO Barebone - Spacy Grey',
+        name: 'matsmart.se',
         type: ActivityType.Watching
     })
 })
+
+
+client.on('messageCreate', async (message) => {
+    if (message.content === '!nocco') {
+            const queryParams = { url: 'https://www.matsmart.se/search?q=nocco' };
+        try {
+            const res = await axios.get('http://localhost:5000/api/m/getproducts', {
+                params: queryParams,
+            });
+            const products = res.data.data.products
+            
+            if (products && products.length > 0) {
+                const embeds = products.map(product => {
+                    return new EmbedBuilder()
+                        .setTitle(product.name)
+                        .setFields(
+                            {
+                                name: 'Brand and Quantity:',
+                                value: product.brandAndQuantity,
+                                inline: true,
+                            },
+                            {
+                                name: 'Price:',
+                                value: `${product.price}kr`,
+                                inline: true,
+                            }
+                        )
+                        .setColor('#c70000')
+                        .setImage(product.imageUrl)
+                });
+
+                await message.reply({ embeds: embeds });
+            } else {
+                await message.reply('No products found');
+            }
+        } catch (error) {
+            console.error('Error on getting the product and sending the response', error);
+        }
+    }
+});
+
 
 
 client.on('messageCreate', async (message) => {
@@ -52,7 +93,7 @@ client.on('messageCreate', async (message) => {
                 .setColor('#c70000')
                 .setImage(product.image)
                 .setThumbnail('https://www.maxgaming.se/themes/maxgaming/design/bilder/maxgaming.png')
-                .setURL(product.url);;
+                .setURL(product.url);
     
                 await message.reply({ embeds: [embed] });
             }
@@ -64,31 +105,7 @@ client.on('messageCreate', async (message) => {
             console.log('error on getting the product and sending the response', error)
         }
     }
-
-    if(message.content == '!check'){
-        try {
-            const queryParams  = { target: 'https://www.maxgaming.se/sv/gaming-tangentbord/q1-qmk-barebone-spacy-grey'}
-
-            const res = await axios.get('http://localhost:5000/api/check', {
-                params: queryParams,
-            })
-
-            const product = res.data.product;
-
-            if (product) {
-                await message.reply(product.title + ' är ' + product.stock);
-            }
-            else {
-                await message.reply('failed to get the product')
-            }
-        }
-        catch(error) {
-            console.log('error on getting the product and sending the response', error)
-        }
-    }
-
 })
-
 
 // /help
 client.on('interactionCreate', async (interaction) => {
@@ -111,7 +128,6 @@ client.on('interactionCreate', async (interaction) => {
                 value: 'Get status',
             }
             )
-
         await interaction.reply({ embeds: [embed], ephemeral: true });
     }
 
